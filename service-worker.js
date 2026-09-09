299833fd31906c80ba4962fdf53ef411ce18fc32
or-taro <rgn1234567@gmail.com>
2026-09-09 17:43:08 +0900

    Refresh Dject Pages cache for workflow UI

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dject-pages-v3';
+const CACHE='dject-pages-v4';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
