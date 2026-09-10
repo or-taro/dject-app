9c73a77d77e7194c6b80e448d1df822187953439
or-taro <rgn1234567@gmail.com>
2026-09-10 11:38:13 +0900

    집필 연결 검증 캐시 갱신

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dject-pages-v8';
+const CACHE='dject-pages-v9';
 const ASSETS=['./','./index.html','./style.css','./app.js','./writing-launch.js','./draft-tools.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
