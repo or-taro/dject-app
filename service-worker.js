3a6ab4fe385a237c0fdc1bce8ebaeef8f342b46d
or-taro <rgn1234567@gmail.com>
2026-09-10 10:47:23 +0900

    집필 시작 수정 캐시 갱신

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='dject-pages-v5';
+const CACHE='dject-pages-v6';
 const ASSETS=['./','./index.html','./style.css','./app.js','./writing-launch.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
